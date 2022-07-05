f1d90771fb9040947772693bfdea375070658cfe
yyNoBug <yaoyuan2000@sjtu.edu.cn>
2022-07-06 03:49:29 +0800

    Modify scripts.

--- scripts/source/vae_nih.bash
+++ scripts/source/vae_nih.bash
@@ -3,9 +3,9 @@ python main_source.py vae_nih \
     --method vae_train \
     --train_list NIH_train \
     --val_list NIH_val \
-    --data_root /export/ccvl12b/yyao/medical_data/NIH \
-    --val_data_root /export/ccvl12b/yyao/medical_data/NIH \
-    --data_path /export/ccvl12b/yyao/medical_data/Multi_all.json \
+    --data_root <Your_data_path> \
+    --val_data_root <Your_data_path> \
+    --data_path data/Multi_all.json \
     --eval_epoch 20 \
     --save_epoch 800 \
-    --max_epoch 2400 \
+    --max_epoch 4800 \
