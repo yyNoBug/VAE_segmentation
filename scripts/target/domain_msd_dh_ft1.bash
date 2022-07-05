b4bdac44a3cc0ebaebf835717ed1654ab133a95e
yyNoBug <yaoyuan2000@sjtu.edu.cn>
2022-07-06 03:59:05 +0800

    Modify scripts.

--- scripts/target/domain_msd_dh_ft1.bash
+++ scripts/target/domain_msd_dh_ft1.bash
@@ -5,8 +5,8 @@ python main_target.py domain_msd_dh_ft1 \
     --load_prefix_vae vae_nih \
     --train_list MSD_train \
     --val_list MSD_val \
-    --data_root <Your_data_path> \
-    --val_data_root <Your_data_path> \
+    --data_root <Your_MSD_data_path> \
+    --val_data_root <Your_MSD_data_path> \
     --data_path data/Multi_all.json \
     --pan_index 10 \
     --lambda_vae 1.0 \
---
+++ scripts/target/domain_syn_pseudo.bash
@@ -0,0 +1,15 @@
+python main_target.py domain_syn_dh_ft1 \
+    -G $1 \
+    --method domain_adaptation \
+    --load_prefix seg_nih \
+    --load_prefix_vae vae_nih \
+    --train_list SYN_train \
+    --val_list SYN_val \
+    --data_root <Your_SYN_data_path> \
+    --val_data_root <Your_SYN_data_path> \
+    --data_path data/Multi_all.json \
+    --pan_index 11 \
+    --lambda_vae 0.0 \
+    --eval_epoch 1 \
+    --save_epoch 100 \
+    --max_epoch 50
